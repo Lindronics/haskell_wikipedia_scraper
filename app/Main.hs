cf11a53f8191ae3109d68b951aa8ee22193c162b
Niklas <niklas@lindorfer.com>
2019-11-27 18:00:19 +0000

    added report and status to LaTeX

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
@@ -8,6 +9,8 @@ import Text.LaTeX
 import Text.LaTeX.Packages.Inputenc
 import Data.Matrix
 import Data.Text (pack)
+import Control.Monad.Trans.Maybe
+import Control.Exception
 
 import WikiScrapeLib
 
@@ -31,13 +34,15 @@ wikify x = "https://en.wikipedia.org/wiki/" ++ x
 
 main :: IO ()
 main = do
-     words <- mapM mostfrequentwordonpage (wikify <$> countries)
-     let results = zip countries words
-     mapM_ (\x -> putStrLn $ (fst x) ++ ": " ++ ((show.snd) x)) results
-     execLaTeXT (tables results) >>= renderFile "tex/tables.tex"
+    words <- mapM mostfrequentwordonpage (wikify <$> countries)
+    let results = zip countries words
+    mapM_ (\x -> putStrLn $ (fst x) ++ ": " ++ ((show.snd) x)) results
+    status <- getReport "status.txt"
+    report <- getReport "report.txt"
+    execLaTeXT (tables results status report) >>= renderFile "tex/tables.tex"
 
-tables :: Monad m => [(String, Maybe String)] -> LaTeXT m ()
-tables l = thePreamble >> document (theBody l)
+tables :: Monad m => [(String, Maybe String)] -> String -> String -> LaTeXT m ()
+tables l status report = thePreamble >> document (theBody l status report)
 
 thePreamble :: Monad m => LaTeXT m ()
 thePreamble = do
@@ -46,14 +51,27 @@ thePreamble = do
     author "Niklas Lindorfer"
     title "Haskell: Most frequent words on each country's Wikipedia page"
 
-theBody :: Monad m => [(String, Maybe String)] -> LaTeXT m ()
-theBody l = do
+theBody :: Monad m => [(String, Maybe String)] -> String -> String -> LaTeXT m ()
+theBody l status report = do
     maketitle
+    section "Status"
+    fromString status
+    section "Execution Results"
     let l_l = fmap ((fmap pack).pairToList) l
     let matr = fromLists l_l
     center $ matrixTabular (fmap textbf ["Country", "Most frequent word"]) matr
+    section "Report"
+    fromString report
+
 
 pairToList :: (String, Maybe String) -> [String]
 pairToList (x,y) = case y of
     Nothing -> [x, "Nothing"]
     Just y -> [x, y]
+
+getReport :: String -> IO String
+getReport s = do
+    contents <- try $ readFile s
+    case contents of
+        Left (e :: SomeException) -> return "*File could not be loaded*"
+        Right content -> return content
